f685fcbbbabebc31b2cdfb620fe46a878ff3b185
Böszörményi Zoltán <zboszor@pr.hu>
2020-04-15 15:55:36 +0200

    Fix build on Fedora 32 with GCC 10 and clang-analyzer (LLVM) 10
    Signed-off-by: Böszörményi Zoltán <zboszor@pr.hu>

--- libsrc/html.c
+++ libsrc/html.c
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2003-2006 SICOM Systems, INC.
+ *  Copyright (C) 2003-2020 SICOM Systems, INC.
  *
  *  Authors:	Bob Doan <bdoan@sicompos.com>
  *
@@ -27,6 +27,7 @@
 #include <config.h>
 
 #include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
